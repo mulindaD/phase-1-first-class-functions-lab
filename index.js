25eabd9aa00033b84d1f71f69bba7c2634725209
Dominic Mulinda <mulindarweshumba@gmail.com>
2024-07-01 18:44:47 +0300

    Complete arrow functions

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = arr => arr.slice(0, 2)
+
+const returnLastTwoDrivers = arr => arr.slice(arr.length - 2)
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = int => {
+    return () => int * int
+}
+
+const fareDoubler = (x) => {
+    return x * 2
+}
+
+const fareTripler = x => x * 3
+
+const selectDifferentDrivers = (arrayOfDrivers, callback) => {
+    return callback(arrayOfDrivers);
+}
+
